3172550dce679454f8c3fc9e285e1b2196b25c56
Claudia Tanaka <catanaka@hotmail.com>
2020-12-01 17:14:29 -0300

    adds style to home and index and seeds db

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,4 +14,68 @@ ingredients.each do |ingredient|
   Ingredient.create!(name: ingredient["strIngredient1"])
   puts "Created new ingredient #{ingredient["strIngredient1"]}."
 end
+
+new_ingr = "Bailey's"
+Ingredient.create!(name: new_ingr)
+puts "Created new ingredient #{new_ingr}."
+
+new_ingr = "Amarula"
+Ingredient.create!(name: new_ingr)
+puts "Created new ingredient #{new_ingr}."
+
+new_ingr = "Blue Curaçao"
+Ingredient.create!(name: new_ingr)
+puts "Created new ingredient #{new_ingr}."
+
+new_ingr = "Cachaça"
+Ingredient.create!(name: new_ingr)
+puts "Created new ingredient #{new_ingr}."
+
+new_ingr = "Raki"
+Ingredient.create!(name: new_ingr)
+puts "Created new ingredient #{new_ingr}."
+
+new_ingr = "Sake"
+Ingredient.create!(name: new_ingr)
+puts "Created new ingredient #{new_ingr}."
+
+new_ingr = "Ice"
+Ingredient.create!(name: new_ingr)
+puts "Created new ingredient #{new_ingr}."
+
+puts "Finished creating ingredients."
+
+
+puts "Creating cocktails"
+
+cocktail_names = [
+  "Caipirinha",
+  "Mojito",
+  "Manhattan",
+  "Gin tonic",
+  "Martini",
+  "Cuba Libre",
+  "Margarita",
+  "Pisco Sour",
+  "Caipiroska",
+  "Bloody Mary",
+  "Sex on the beach",
+  "Piña Colada",
+  "Spritz",
+  "White Russian",
+  "Shirley Temple",
+  "Strawberry Daiquiri",
+  "Mint Julep"
+]
+
+cocktail_names.each_with_index do |name, index|
+  file_name = "#{index + 1}.jpg"
+  file_path = "app/assets/images/card_imgs/#{file_name}"
+
+  cocktail = Cocktail.new(name: name)
+  cocktail.photo.attach(io: File.open(Rails.root + file_path), filename: file_name, content_type: 'image/jpg')
+  cocktail.save!
+  puts "Created new cocktail #{name} with photo."
+end
+
 puts "Finished!"
