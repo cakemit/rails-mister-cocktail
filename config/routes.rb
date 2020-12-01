a658954257221ffaa25845ec588f3896ead192a0
Claudia Tanaka <catanaka@hotmail.com>
2020-12-01 13:48:54 -0300

    add dotenv - Protect my private data in .env file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'cocktails#index'
+  get 'pages/home'
+  root to: 'pages#home'
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
